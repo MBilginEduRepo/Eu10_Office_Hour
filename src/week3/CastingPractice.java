a010b0d5ca9a8465d44f5e8a7e3a83c85c098759
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-11-02 20:52:17 +0300

    week 3 first push

---
+++ src/week3/CastingPractice.java
@@ -0,0 +1,13 @@
+package week3;
+
+public class CastingPractice {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = a;
+
+        double c = 100;
+        int d = (int)c;
+    }
+}
