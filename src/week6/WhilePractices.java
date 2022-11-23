53f8064cfbea1b1192e18ddef2dfb9f26bbbcd53
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-11-23 22:36:50 +0300

    week 6 second push

--- src/week6/WhilePractices.java
+++ src/week6/WhilePractices.java
@@ -1,11 +1,16 @@
 package week6;
 
+import java.util.*;
+
 public class WhilePractices {
 
     public static void main(String[] args) {
 //        one();
 //        two();
-        three();
+//        three();
+//        four();
+//        practice5();
+        six();
     }
 
     public static void one() {
@@ -39,5 +44,51 @@ public class WhilePractices {
                 System.out.print(" " + i);
 //            i++;
         } while (i < 11);
+    }
+
+    public static void four() {
+        for (int i = 1; i < 50; i++) {
+            if (i % 5 != 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+
+    // print only vowels in given word
+    // a, e, i, o, u      home --> o, e
+
+    public static void practice5(){
+        String data = "";
+        String str="abcdefghijklmnopqrstuvwxyz";
+        String result="";
+        for (int i = 0; i <str.length() ; i++) {
+            char ch=str.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='u'||ch=='i'||ch=='o'){
+                result+=ch;            }
+        }
+        System.out.println(result);
+    }
+
+    // while loop password checker, 3 guess
+    public static void six(){
+        String password = "Cydeo123!";
+        Scanner scanner = new Scanner(System.in);
+        int guessNo = 0;
+
+        while (true){
+            System.out.println("Please enter your password");
+            String guess = scanner.nextLine();
+            if (guess.equals(password)){
+                System.out.println("Welcome to dashboard");
+                break;
+            }
+            guessNo++;
+            if(guessNo == 3){
+                System.out.println("Your account is deactivated");
+                break;
+            }
+        }
     }
 }
