0236fe4897bc5156fea4732654a12a5a11e5a801
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-12-07 20:59:23 +0300

    week 8 first push

---
+++ src/week7/MultiDArraysPractice.java
@@ -0,0 +1,11 @@
+package week7;
+
+public class MultiDArraysPractice {
+
+    public static void main(String[] args) {
+
+    }
+
+    // create a 2d array of fruits, has fruits, price, count
+    // print --> apple price is 10 dollar, 100 left.
+}
