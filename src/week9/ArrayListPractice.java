0ec9ddf7288d1df10e251713f81deb65662b1519
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-12-21 22:34:19 +0300

    week 9 third push

---
+++ src/week9/ArrayListPractice.java
@@ -0,0 +1,84 @@
+package week9;
+
+import java.util.*;
+
+public class ArrayListPractice {
+
+    public static void main(String[] args) {
+
+//        one();
+//        two();
+        three();
+    }
+
+    //1.Write a Java program that creates an ArrayList of strings and then adds five strings to the list.
+    // Then remove the first and last elements of the list, and then print the remaining elements to the console.
+
+    public static void one() {
+        ArrayList<String> fruits = new ArrayList<>();  // arraylist only receives primitive/wrapper class / object data type
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("pear");
+        fruits.add("grape");
+        fruits.add("ananas");
+
+        System.out.println(fruits);  // arraylist can be printed directly
+
+        fruits.remove(0);
+        fruits.remove(fruits.size() - 1);
+
+        System.out.println(fruits);
+    }
+
+    //2.Write a Java program that creates an ArrayList of 10 integers, and then
+    // removes the elements at index positions 2, 4, and 6 from
+    //the list. The program should then print the resulting list to the console.
+
+    // Array to arraylist  --> use add all method, arrayList.addAll(arrays)
+    // Arraylist to array -->  no need to do that, arraylist works as arrays inside
+
+    public static void two() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < 11; i++) {
+            numbers.add(i);
+        }
+
+        System.out.println(numbers);
+
+        numbers.remove(2);
+        System.out.println(numbers);
+
+        numbers.remove(4);
+        System.out.println(numbers);
+
+        numbers.remove(6);
+
+        System.out.println(numbers);
+
+    }
+
+//    3.Write a java program that create an arraylist has random numbers, then check those number,
+//    if they are even, add again to the arraylist
+
+    public static void three() {
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            randomNumbers.add(random.nextInt(100));
+        }
+
+        System.out.println(randomNumbers);
+
+        for (int num : randomNumbers) {
+            if (num % 2 == 0){
+                randomNumbers.add(num);
+            }
+        }
+
+        System.out.println(randomNumbers);
+
+    }
+
+//    4.Write a Java program that creates an ArrayList of 10 random integers between 1 and 100,
+//    and then sorts the list in ascending order.
+}
