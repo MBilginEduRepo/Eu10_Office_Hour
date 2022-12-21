a571c87bf3162234a9ef695408a00ce3a39a7410
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-12-21 21:43:43 +0300

    week 9 second push

--- src/week9/WrapperClassMethods.java
+++ src/week9/WrapperClassMethods.java
@@ -6,8 +6,8 @@ public class WrapperClassMethods {
 
     public static void main(String[] args) {
 
-        one();
-
+//        one();
+        two();
         // autoboxing
         int a = 40;
         Integer b = a;
@@ -25,10 +25,10 @@ public class WrapperClassMethods {
 
 //        System.out.println(Integer.toString(123).concat("OneTwoThree"));  // "123"
 
-        String str = "123house258";
+        String str = "123house2589936";
         int total = 0;
         for (int i = 0; i < str.length(); i++) {
-            if(Character.isDigit(str.charAt(i))){
+            if (Character.isDigit(str.charAt(i))) {
                 total += Integer.valueOf("" + str.charAt(i));
             }
         }
@@ -36,7 +36,7 @@ public class WrapperClassMethods {
 //        System.out.println("total = " + total);
     }
 
-    public static void one(){
+    public static void one() {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a number:");
@@ -45,10 +45,24 @@ public class WrapperClassMethods {
         boolean isPositive = false;
 
         int num = Integer.parseInt(number); //"123"
-        if(num >= 0){
+        if (num >= 0) {
             isPositive = true;
         }
 
         System.out.println(isPositive);
+    }
+
+    public static void two() {
+        String str = "456number87459963";
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                if (Integer.valueOf(str.charAt(i)) % 3 == 0) {
+                    sum += Integer.valueOf("" + str.charAt(i));
+                }
+            }
+        }
+
+        System.out.println(sum);
     }
 }
