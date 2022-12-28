fdbd407a3e231844db51d58f8993d4e8d3e5391a
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-12-28 22:47:18 +0300

    week 10 third push

---
+++ src/week10/constructors/PhoneObject.java
@@ -0,0 +1,11 @@
+package week10.constructors;
+
+public class PhoneObject {
+
+    public static void main(String[] args) {
+
+        Phone apple = new Phone("Apple","XS",256, 1299.99);
+        System.out.println(apple);
+
+    }
+}
