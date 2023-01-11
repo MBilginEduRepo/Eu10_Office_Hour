1da70dc3578c36c1ec6148846922964800aa0fa9
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-01-11 22:28:17 +0300

    week 12 first push

---
+++ src/week12/encapsulation/Bank.java
@@ -0,0 +1,13 @@
+package week12.encapsulation;
+
+public class Bank {
+
+
+
+    BankAccount account1 = new BankAccount();
+
+    public void one(){
+        System.out.println(account1.getAccountName());
+        account1.setAccountName("John");
+    }
+}
---
+++ src/week12/encapsulation/BookInfo.java
@@ -0,0 +1,17 @@
+package week12.encapsulation;
+
+public class BookInfo {
+
+    // bookName
+    private String bookName;
+    // year
+    private int year;
+    // ISSN
+    private long ISSN;
+    // borrowd or not
+
+
+
+
+
+}
